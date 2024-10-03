ac396843aefd0691508c6f2d0884436ac8d11e99
ravi0629prakash <raviprakash0629@gmail.com>
2024-10-03 19:20:20 +0530

    4th COMMIT

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,14 @@
- 
+
+import Hero from '@/components/Hero';
+import InfoBoxes from '@/components/InfoBoxes';
+import HomeProperties from '@/components/HomeProperties';
 const Homepage = () => {
   return (
-    <div>
-      <h1 className='text-3xl'>Welcome</h1>
-      <a href='/properties'>Show Properties</a>
-     
-    </div>
+    <> 
+       <Hero /> 
+       <InfoBoxes /> 
+       <HomeProperties /> 
+    </>
   );
 };
 
--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,11 +1,21 @@
-
+import properties from '@/properties.json';
+import PropertyCard from '@/components/PropertyCard';
 const PropertiesPage = () => {
   return (
-    <div>
-        <h1 className='text-3xl'>Properties</h1>
-        <a href='/'>Go Home</a>
+    <section className= "px-4 py-6">
+      <div className= "container-xl lg:container m-auto px-4 py-6">
+        {properties.length=== 0 ? (
+          <p>No Properties Found </p>
+        ) : ( 
+        <div className= "grid grid-cols-1 md:grid-cols-3 gap-6">
+          { properties.map((property)=> (
+           <PropertyCard Key={property._id} property={property} / >
+          ))}
+          </div>)}
+       
     </div>
-  )
-}
+    </section>
+  );
+};
 
 export default PropertiesPage;
