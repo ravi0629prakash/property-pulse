cce7f3cd1f6729cbd9678c72f81aecd9715315cc
ravi0629prakash <raviprakash0629@gmail.com>
2024-10-04 11:29:57 +0530

    MongoDB

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import Hero from '@/components/Hero';
 import InfoBoxes from '@/components/InfoBoxes';
 import HomeProperties from '@/components/HomeProperties';
 const Homepage = () => {
+  // console.log(process.env.MONGODB_URI);
   return (
     <> 
        <Hero /> 
