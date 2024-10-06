fa1db329217517114cbe5171c06ea996483782ee
ravi0629prakash <raviprakash0629@gmail.com>
2024-10-07 00:02:00 +0530

    Working till delete and add property .. before tostify

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -1,12 +1,14 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
-//GET /a[i/properties]
+import { getSessionUser } from "@/utils/getSessionUser";
+//GET /api/properties]
 export const GET = async(request ,{params})=>{
+    
     try{
       await connectDB();
 
       const property=await Property.findById(params.id);
-  
+      
       if(!property)
         return new Response('Property Not Found' , {status:404});
 
@@ -15,6 +17,44 @@ export const GET = async(request ,{params})=>{
     catch(error)
     {
      console.log(error);
-     return new Response('Something Went Wrong', {status:500});
+     return new Response('Something Wen2 Wrong', {status:500});
     } 
-}
+};
+
+//DELETE/api/properties]
+
+export const DELETE = async(request ,{params})=>{
+    
+  try{
+    const propertyId=params.id;
+    const sessionUser = await getSessionUser();
+    //check for session
+    if(!sessionUser || !sessionUser.userId){
+      return new Response('User Id is Required' , {status:401});
+    }
+
+    const{userId}=sessionUser;
+
+    await connectDB();
+
+    const property=await Property.findById(propertyId);
+    
+    if(!property)
+      return new Response('Property Not Found' , {status:404});
+
+    //Verify Ownership
+    if(property.owner.toString()!=userId)
+    {
+      return new Response('Unauthorized' , {status:401});
+    }
+
+    await property.deleteOne();
+    return new Response('Property Deleted' , {status:200});
+
+  }
+  catch(error)
+  {
+   console.log(error);
+   return new Response('Something Wen2 Wrong', {status:500});
+  } 
+};
